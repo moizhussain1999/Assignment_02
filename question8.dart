bc00ed3e6407191f7db09e07f47faf265faa200b
Moiz Hussain <moiz.hussain1999@gmail.com>
2024-12-12 10:12:43 -0800

    Assing,ent no 2

--- question8.dart
+++ question8.dart
@@ -1,9 +1,27 @@
-
 void main(){
 
-  List data = [
+//Q.8: remove all false values from below list by using removeWhere or retainWhere property.
+//List<Map<String, bool>> usersEligibility = [
+//{'name': 'John', 'eligible': true},
+//{'name': 'Alice', 'eligible': false},
+//{'name': 'Mike', 'eligible': true},
+//{'name': 'Sarah', 'eligible': true},
+//{'name': 'Tom', 'eligible': false},
+//];
 
-    
-  ];
-  
-  }
+List<Map<dynamic,dynamic>> usersEligibility = [
+
+{'name': 'John', 'eligible': true},
+{'name': 'Alice', 'eligible': false},
+{'name': 'Mike', 'eligible': true},
+{'name': 'Sarah', 'eligible': true},
+{'name': 'Tom' , 'eligible': false},
+
+];
+print(usersEligibility);
+
+usersEligibility.removeWhere((element) => element.containsValue(false));
+
+print(usersEligibility);
+
+}
